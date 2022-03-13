07df36156f239f888e49698781c3b8a7a5b0e9bf
mana <manashams94@gmail.com>
2022-03-13 16:07:49 +0330

    NoteDetailView & its url added.

--- Notebook/note/views.py
+++ Notebook/note/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import edit, list, UpdateView, base
+from django.views.generic import edit, list, UpdateView
 from django.urls import reverse
 from .models import Note
 from .forms import NoteForm
@@ -9,6 +9,11 @@ class NoteListView(list.ListView):
     model = Note
     queryset = Note.objects.order_by('-updated_at')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = NoteForm()
+        return context
+
 
 class NoteCreateView(edit.CreateView):
     template_name = 'index.html'
@@ -17,13 +22,20 @@ class NoteCreateView(edit.CreateView):
     def get_success_url(self):
         return reverse('note:note-list')
 
+
 class NoteUpdateView(UpdateView):
     template_name = 'index.html'
     form_class = NoteForm
     queryset = Note.objects.all()
 
+
 class NoteDeleteView(edit.DeleteView):
     template_name = 'index.html'
     form_class = NoteForm
+    queryset = Note.objects.all()
 
+
+class NoteDetailView(edit.DeleteView):
+    template_name = 'index.html'
+    model = Note
 
