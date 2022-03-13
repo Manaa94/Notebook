d5b7da55eef6ca4efb968f5795dbf2521a807881
mana <manashams94@gmail.com>
2022-03-14 01:53:32 +0330

    NoteDetailView added - some changes in view.

--- Notebook/note/views.py
+++ Notebook/note/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import edit, list, UpdateView
+from django.views.generic import edit, list, UpdateView, DetailView
 from django.urls import reverse
 from .models import Note
 from .forms import NoteForm
@@ -7,7 +7,7 @@ from .forms import NoteForm
 class NoteListView(list.ListView):
     template_name = 'index.html'
     model = Note
-    queryset = Note.objects.order_by('-updated_at')
+    queryset = Note.objects.order_by('-updated_at')[:10]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -28,14 +28,38 @@ class NoteUpdateView(UpdateView):
     form_class = NoteForm
     queryset = Note.objects.all()
 
+    def get_success_url(self):
+        return reverse('note:note-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['update'] = 'update'
+        context['object_list'] = Note.objects.order_by('-updated_at')[:10]
+        return context
+
 
 class NoteDeleteView(edit.DeleteView):
     template_name = 'index.html'
-    form_class = NoteForm
-    queryset = Note.objects.all()
+    model = Note
+
+    def get_success_url(self):
+        return reverse('note:note-list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['delete'] = 'Are you sure?'
+        context['form'] = NoteForm()
+        context['object_list'] = Note.objects.order_by('-updated_at')[:10]
+        return context
 
 
-class NoteDetailView(edit.DeleteView):
+class NoteDetailView(DetailView):
     template_name = 'index.html'
     model = Note
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['detail'] = 'detail'
+        context['form'] = NoteForm()
+        context['object_list'] = Note.objects.order_by('-updated_at')[:10]
+        return context
