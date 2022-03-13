afc85f05b8594521abfdca62fb8cbcec8a7e40d4
mana <manashams94@gmail.com>
2022-03-13 15:13:44 +0330

    NoteCreateView & its url added.

--- Notebook/note/views.py
+++ Notebook/note/views.py
@@ -1,8 +1,18 @@
-from django.views.generic import list
+from django.views.generic import edit, list
+from django.urls import reverse
 from .models import Note
+from .forms import NoteForm
 
 
 class NoteListView(list.ListView):
     template_name = 'index.html'
     model = Note
     queryset = Note.objects.order_by('-updated_at')
+
+
+class NoteCreateView(edit.CreateView):
+    template_name = 'index.html'
+    form_class = NoteForm
+
+    def get_success_url(self):
+        return reverse('note:note-list')
